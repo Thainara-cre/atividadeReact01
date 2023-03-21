f624c4e51c73877783785e6f3fd8c28b84c5bfa4
Thainara da Silva <thainaracaracol@gmail.com>
2023-03-20 22:50:22 -0300

    subindo a lição

---
+++ atividade/src/App.jsx
@@ -0,0 +1,50 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div classeName="Card">
+          <div classeName="Card1">
+            <h1>
+              Básico
+            </h1>
+            <strong>
+                    $5
+            </strong>
+            <h2>por mês</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <button>Compre Agora</button>
+         </div>
+         <div>
+            <h1>
+              O negócio
+            </h1>
+            <strong>
+                    $45
+            </strong>
+            <h2>por mês</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <button>Compre Agora</button>
+          </div>
+          <div>
+            <h1>
+              Profissional
+            </h1>
+            <strong>
+                    $95
+            </strong>
+            <h2>por mês</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </p>
+            <button>Compre Agora</button>
+          </div>
+       </div>
+  )
+}
+
+export default App
---
+++ atividade/src/Card.jsx
@@ -0,0 +1,27 @@
+function Card(){
+    return(
+       <div classeName="Card">
+         <div>
+            <h1>
+                <strong>
+                    <p><button>Compre Agora</button></p>
+                </strong>
+            </h1>
+         </div>
+         <div>
+            <h1>
+                <strong>
+                    <p><button>Compre Agora</button></p>
+                </strong>
+            </h1>
+         </div>
+         <div>
+            <h1>
+                <strong>
+                    <p><button>Compre Agora</button></p>
+                </strong>
+            </h1>
+         </div>
+       </div>
+    )
+}
